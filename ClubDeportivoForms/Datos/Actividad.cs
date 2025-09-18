5291ed700bd1fb0659f390e0d16b0bfb7a8b2322
Axel Benjamín Alvarez Alcaraz <axelalvarez0200@gmail.com>
2025-09-18 18:39:27 -0300

    Actividad.cs actualizado

--- ClubDeportivoForms/Datos/Actividad.cs
+++ ClubDeportivoForms/Datos/Actividad.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ClubDeportivoForms.Datos {
-    internal static class  Actividad {
+    internal static class Actividad {
         public static List<string> CargarActividades() {
             List<string> Actividades = new List<string>();
             MySqlConnection sqlCon = new MySqlConnection();
@@ -18,7 +18,130 @@ namespace ClubDeportivoForms.Datos {
                 string query;
                 sqlCon = Conexion.getInstancia().CrearConexion();
                 query = "SELECT nombreActividad FROM actividades;";
+
+                MySqlCommand comando = new MySqlCommand(query, sqlCon);
+                comando.CommandType = CommandType.Text;
+                sqlCon.Open();
+
+                MySqlDataReader reader;
+
+                reader = comando.ExecuteReader();
+
+                if( reader.HasRows) {
+                    while (reader.Read()) {
+                        Actividades.Add(reader["nombreActividad"].ToString());
+                    }
+                } catch( Exception ex) {
+                    MessageBox.Show(ex.Message, "MENSAJE DEL CATCH", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                } finally {
+                    if(sqlCon.State == ConnectionState.Open) 
+                    {
+                        sqlCon.Close();
+                    }
+                }
+
+                return Actividades;
+
+                
+            }
+
+            public static int RevisarCupo(string actividad) {
+                MySqlConnection sqlCon = new MySqlConnection();
+                int cupo = 0;
+
+                try {
+                    string query;
+                    sqlCon = Connection.getInstancia().CrearConexion();
+                    query = ("SELECT cupoDisponible FROM actividades WHERE nombreActividad = '" + actividad + "'");
+
+                    MySqlComand comando = new MySqlCommand(query, sqlCon);
+                    comando.CommandType = CommandType.Text;
+                    sqlCon.Open();
+
+                    MySqlDataReader reader;
+
+                    reader = comando.ExecuteReader();
+
+                    if (reader.HasRows) {
+                        reader.Read();
+                        cupo = reader.GetInt32(0);
+                    }
+
+
+                } catch  (Exception ex) {
+                    MessageBox.Show(ex.Message, "MENSAJE DEL CATCH", MessageBoxButtons.OK, MessageBoxButtons.Error);
+
+                } finally {
+                    if(sqlCon.State == ConnectionState.Open) {
+                        sqlCon.Close();
+                    }
+
+                }
+
+                return cupo;
+                
+            }
+
+            public static void RestarCupo(string actividad) {
+                MySqlConnection sqlCon = new MySqlConnection();
+
+                try {
+                    sqlCon = Conexion.getInstancia().CrearConexion();
+                    MySqlCommand comando = new MySqlCommand("restarCupo", sqlCon);
+                    comando.CommandType = CommandType.StoredProcedure;
+
+                    comando.Parameters.Add("actividad", MySqlDbType.Varchar).Value = actividad;
+
+                    sqlCon.Open();
+                    comando.ExecuteNonQuery();
+
+                } catch(Exception ex) {
+                    MessageBox.Show(ex.Message, "MENSAJE DEL CATCH");
+
+                } finally {
+                    if(sqlCon.State == ConnectionState.Open) {
+                        sqlCon.Close();
+                    }
+                }
+            }
+
+            public static double BuscarPrecio(string actividad) {
+                MySqlConnection sqlCon = new MySqlConnection();
+                double precio = 0;
+
+                try {
+                    string query;
+
+                    sqlCon = Conexion.getInstancia().CrearConexion();
+
+                    query = ("SELECT precioDiario FROM actividades WHERE nombreActividad =" + "'" + actividad + "'");
+
+                    MySqlCommand comando = new MySqlCommand(query, sqlCon);
+                    comando.CommandType = CommandType.Text;
+
+                    sqlCon.Open();
+                    MySqlDataReader reader;
+
+                    reader = comando.ExecuteReader();
+
+                    reader.Read();
+
+                    precio = reader.GetDouble(0);
+
+                } catch(Exception ex) {
+                    MessageBox.Show(ex.Message, "MENSAJE DEL CATCH...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                } finally {
+                    if(sqlCon.State == ConnectionState.Open) {
+                        sqlCon.Close();
+                    }
+                }
+
+                return precio;
             }
         }
+
+
     }
 }
